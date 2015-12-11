a376a51a0bab62832eb9420892c1c23a9dc4003d
AD Slaton <slaton.ad@gmail.com>
2015-12-11 09:26:43 -0500

    initial commit

---
+++ portal/site/js/login.js
@@ -0,0 +1,236 @@
+/* 11/30/2015 */
+var COPA = COPA || {},
+    api = 'http://www.copaseguridad.com/authenticate/',
+    day = 86400000;
+
+jQuery.support.cors = true;
+
+function emailDomainCheck(email, domain) {
+    var parts = email.split('@');
+    if (parts.length === 2) {
+        if (parts[1] === domain) {
+            return true;
+        }
+    }
+    return false;
+}
+
+COPA.Authenticate = {
+    set: function (cname, cvalue, exdays) {
+        var d = new Date(),
+            expires;
+        
+        d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+        expires = 'expires=' + d.toUTCString();
+        document.cookie = cname + '=' + cvalue + '; ' + expires + '; domain=.copaseguridad.com;path=/';
+    },
+    get: function (cname) {
+        var name = cname + '=',
+            ca = document.cookie.split(';'),
+            i,
+            c;
+        
+        for(i = 0; i < ca.length; i++) {
+            c = ca[i];
+            while (c.charAt(0) === ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) === 0) {
+                return c.substring(name.length, c.length);
+            }
+        }
+        return '';
+    },
+    remove: function () {
+        document.cookie = "user=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
+    },
+    register: function (auth) {
+	    jQuery.support.cors = true;
+        var userMessage = '';
+        jQuery.ajax({
+            url: api + 'register',
+            method: 'POST',
+            data: JSON.stringify(auth),
+            dataType: 'json',
+            processData: false,
+            success: function (data) {
+                console.log(data);
+                if (data && data.authenticate && data.register) {
+                    jQuery('.auth').hide();
+                    jQuery('.registered').show();
+                } else {
+                    userMessage = data.error + '<br><a href="login.html"> LOGIN</a> or <a href="reset.html"> RESET PASSWORD</a>'
+                    jQuery('.validate.user').html(userMessage).show();
+                }
+            },
+            error: function (jqXHR, status, error) {
+                console.log(jqXHR.responseText);
+                console.log(status);
+                console.log(error);
+            }
+        });
+    },
+    reset: function (auth) {
+        jQuery.support.cors = true;
+        jQuery.ajax({
+            url: api + 'reset',
+            method: 'POST',
+            data: JSON.stringify(auth),
+            dataType: 'json',
+            processData: false,
+            success: function (data) {
+                console.log(data);
+                if (data && data.authenticate) {
+                    COPA.Authenticate.set(
+                        'user',
+                        data.username,
+                        day
+                    );
+                    jQuery('.reseter').remove();
+                    jQuery('.registered').show();
+                    jQuery('.auth').hide();
+                    jQuery('.menuItem .auth').html('<a href="login.html">Login</a>');
+                }
+            },
+            error: function (jqXHR, status, error) {
+                console.log(jqXHR.responseText);
+                console.log(status);
+                console.log(error);
+            }
+        });
+    },
+    login: function (auth) {
+        var message = '* Email address or password was incorrect';
+        jQuery.support.cors = true;
+        jQuery.ajax({
+            url: api + 'login',
+            method: 'POST',
+            data: JSON.stringify(auth),
+            dataType: 'json',
+            processData: false,
+            success: function (data) {
+                jQuery('.spin').hide();
+                console.log(data);
+                if (data && data.authenticate && data.login) {
+                    COPA.Authenticate.set(
+                        'user',
+                        data.username,
+                        day
+                    );
+                    jQuery('.success').show();
+                    location.href = 'index.html';
+                } else {
+                    jQuery('.spin').hide();
+                    jQuery('.auth').show();
+                    jQuery('.validate.user').html(message).show();
+                }
+            },
+            error: function (jqXHR, status, error) {
+                jQuery('.spin').hide();
+                jQuery('.auth').show();
+                jQuery('.validate.user').html(message).show();
+                console.log(jqXHR.responseText);
+                console.log(status);
+                console.log(error);
+            }
+        });
+    },
+	validate: function (type) {
+        var user = jQuery('#user').val(),
+            domains = {
+                copa: 'copaair.com',
+                mass: 'masoftwaresystems.com',
+                gmail: 'gmail.com'
+            },
+            isValid = (emailDomainCheck(user, domains.copa) || emailDomainCheck(user, domains.mass)),
+            message = '';
+
+        switch (type) {
+            case 'login':
+                if (!isValid) {
+                    message = '* Copa Air email address required';
+                }
+                break;
+            case 'register':
+                if (!isValid) {
+                    message = '<p>The email address you entered does not appear to be a valid copaair.com email address. ' +
+                    'Re-enter your email address.</p>' +
+                    '<p>Questions? Send an email to <a href="mailto:seguridad@copaair.com">seguridad@copaair.com</a></p>';
+                }
+                break;
+            case 'reset':
+                isValid = true;
+                break;
+        }
+
+		jQuery('.validate.user').html('').hide();
+
+		if (!isValid) {
+			jQuery('.validate.user').html(message).show();
+            jQuery('.auth').show();
+            jQuery('.spin').hide();
+            return false;
+		}
+        return true;
+	},
+    init: function () {
+        jQuery('.menuItem .auth').html('<a href="login.html">Login</a>');
+        if (COPA.Authenticate.get('user') !== null && COPA.Authenticate.get('user') !== '') {
+            var user = COPA.Authenticate.get('user').split('@')[0];
+            jQuery('.menuItem .auth').html('<a href="profile.html">Welcome, ' + user + '</a>');
+            jQuery('.username').html(COPA.Authenticate.get('user'));
+            jQuery('.home').show();
+        } else {
+            if (location.pathname.indexOf('login.html') === -1 &&
+                location.pathname.indexOf('register.html') === -1 &&
+                location.pathname.indexOf('reset.html') === -1) {
+                location.href = 'login.html';
+            } else {
+                jQuery('.home').show();
+            }
+        }
+        jQuery('#login').click(function () {
+            jQuery('.spin').show();
+            jQuery('.auth').hide();
+            if (COPA.Authenticate.validate('login')) {
+                COPA.Authenticate.login({
+                    username: jQuery('#user').val(),
+                    password: jQuery('#token').val()
+                });
+            }
+        });
+
+        jQuery('#register').click(function () {
+            var N = 10,
+                token = Array(N+1).join((Math.random().toString(36)+'00000000000000000').slice(2, 18)).slice(0, N);
+
+            if (COPA.Authenticate.validate('register')) {
+                COPA.Authenticate.register({
+                    username: jQuery('#user').val(),
+                    password: token
+                });
+            }
+        });
+
+        jQuery('#reset').click(function () {
+            var N = 10,
+                token = Array(N+1).join((Math.random().toString(36)+'00000000000000000').slice(2, 18)).slice(0, N);
+
+            if (COPA.Authenticate.validate('reset')) {
+                COPA.Authenticate.reset({
+                    username: jQuery('#user').val() || COPA.Authenticate.get('user'),
+                    password: token
+                });
+            }
+        });
+
+        jQuery('#logout').click(function () {
+            COPA.Authenticate.remove();
+            location.href = 'login.html';
+        });
+    }
+};
+
+jQuery(document).ready(function () {
+    COPA.Authenticate.init();
+});
---
+++ portal/site/search.php
@@ -0,0 +1,372 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Search Results</title>
+  <meta charset="utf-8">
+  <meta name = "format-detection" content = "telephone=no" />
+  <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+
+  <link rel="stylesheet" href="css/grid.css">
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/animate.css">
+
+  <link href='http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900' rel='stylesheet' type='text/css'>
+  <link href='http://fonts.googleapis.com/css?family=Roboto:100,300,400,700,900' rel='stylesheet' type='text/css'>
+  <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+  <script src="js/jquery.js"></script>
+  <script src="js/jquery-migrate-1.2.1.js"></script>
+  <script src="js/jquery.easing.1.3.js"></script> 
+  <script src="search/search.js"></script>
+
+  <script src="//maps.googleapis.com/maps/api/js?v=3.exp&amp;sensor=false"></script> 
+
+  <!--[if (gt IE 9)|!(IE)]><!-->
+    <script src="js/jquery.mobile.customized.min.js"></script>
+  <!--<![endif]-->
+  <script>
+    $(document).ready(function () {
+      var state = 0;
+      document.getElementById('icon').onclick = function() {
+        if (state == 1) {
+          document.getElementById('search').style.display = 'none';
+          state = 0;
+          return;
+        }
+        document.getElementById('search').style.display = 'block';
+        state = 1;
+      };
+    });
+  </script>
+  <!--[if lt IE 8]>
+  <div id="ie6-alert" style="width: 100%; text-align:center;">
+    <img src="http://beatie6.frontcube.com/images/ie6.jpg" alt="Upgrade IE 6" width="640" height="344" border="0" usemap="#Map" longdesc="http://die6.frontcube.com" />
+      <map name="Map" id="Map"><area shape="rect" coords="496,201,604,329" href="http://www.microsoft.com/windows/internet-explorer/default.aspx" target="_blank" alt="Download Interent Explorer" /><area shape="rect" coords="380,201,488,329" href="http://www.apple.com/safari/download/" target="_blank" alt="Download Apple Safari" /><area shape="rect" coords="268,202,376,330" href="http://www.opera.com/download/" target="_blank" alt="Download Opera" /><area shape="rect" coords="155,202,263,330" href="http://www.mozilla.com/" target="_blank" alt="Download Firefox" />
+        <area shape="rect" coords="35,201,143,329" href="http://www.google.com/chrome" target="_blank" alt="Download Google Chrome" />
+      </map>
+  <![endif]-->
+
+  <!--[if lt IE 9]>
+    <script src="js/html5shiv.js"></script>
+    <link rel="stylesheet" type="text/css" media="screen" href="css/ie.css">
+  <![endif]-->
+</head>
+
+<body>
+  <!--========================================================
+                            HEADER 
+  =========================================================-->
+  <header id="header">
+    <section class="header_top">
+      <div id="stuck_container">
+
+        <h1><a href="index.html">&nbsp; <span class="light">&nbsp;</span></a></h1>
+
+        <div class="search-block" id="search-block">
+          <div class="search-block_icon fa fa-search" id="icon"></div>
+          <form id="search" style="display: none;" action="search.php" method="GET" accept-charset="utf-8">
+            <input type="text" name="s" placeholder="Enter keyword:"/>
+            <a onclick="document.getElementById('search').submit()"><div class="search_icon"></div>
+            </a>
+          </form>
+        </div>
+    
+        <div class="navigation">
+            <nav>
+              <ul class="menu">
+                <li><a href="http://copaseguridad.com/webreport2.aspx" target="_blank"
+ 
+                    href2="#link1">FILE A REPORT&nbsp;&nbsp;|</a></li>
+                <li><a href="http://copaseguridad.com/birdstrikereport2.aspx" target="_blank"
+ 
+                    href2="#link1">FILE A BIRD OR WILDLIFE STRIKE
+                    REPORT&nbsp;&nbsp;|</a></li>
+                <li><a href="http://copaseguridad.com/portal/site/index.html#facts">SOME
+                    FACTS&nbsp;&nbsp;|</a></li>
+                <li><a href="http://copaseguridad.com/portal/site/index.html#warriors">SAFETY
+                    WARRIORS&nbsp;&nbsp;|</a></li>
+                <li><a href="http://copaseguridad.com/portal/site/index.html#askevita">SAFETY
+                    INFORMATION</a></li>
+			<table class="menutable" border="0" cellpadding="0" cellspacing="0" border="0">
+			<tr><td height="10"></td></tr>
+			<tr><td rowspan="3" width="30">
+			<img src="images/evita.png" height="45" style2="padding-left:5px;padding-top:15px;padding-right:50px;" />
+			</td>
+			<td width="20" style="padding-bottom:3px;"><a href="index_panama.html"><img src="images/panama-flag.jpg" width="20" border="0" /></a></td>
+			</tr><tr>
+			<td><a href="index.html"><img src="images/usa.png" width="20" border="0" /></a></td>
+			</tr><tr>
+			<td><a href="index_brazil.html"><img src="images/brazil.png" width="20" border="0" /></a></td>
+			</tr>
+			</table>
+            </nav>
+          </div>
+        </div>
+
+      </div>
+    </section>
+  </header>
+
+  <!--========================================================
+                            CONTENT 
+  =========================================================-->
+<section id="content">
+  <article>
+    <div class="container">
+      <div class="search_results">
+        <div class="row">
+          <div class="grid_12">
+            <h2>Search results</h2>
+            <div id="search-results"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </article>
+</section>
+
+
+ <!--========================================================
+                            FOOTER 
+  =========================================================-->
+  <footer id="footer">
+    <div class="footer_copyright text-3" style="font-size:10px;">&copy;2015 MA Software Systems.  All Rights Reserved.</div><!--
+  <section class="content_map">
+    <div class="google-map-api"> 
+      <div id="map-canvas" class="gmap"></div> 
+    </div>
+  </section>
+-->
+  </footer>
+
+  <script src="js/script.js"></script>
+  <script type="text/javascript"> 
+  google_api_map_init(); 
+  function google_api_map_init(){ 
+    var map; 
+    var coordData = new google.maps.LatLng(parseFloat(40.646197), parseFloat(-73.9724068,14)); 
+    var marker; 
+  
+    var styleArray = [
+    {
+        "featureType": "water",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#d3d3d3"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "stylers": [
+            {
+                "color": "#808080"
+            },
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "color": "#b3b3b3"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "color": "#ffffff"
+            },
+            {
+                "weight": 1.8
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#d7d7d7"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "color": "#ebebeb"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#a7a7a7"
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "color": "#efefef"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#696969"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "color": "#737373"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#d6d6d6"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {},
+    {
+        "featureType": "poi",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#dadada"
+            }
+        ]
+    }
+]
+               
+    var markerIcon = { 
+        url: "images/gmap_marker.png", 
+        size: new google.maps.Size(42, 65), 
+        origin: new google.maps.Point(0,0), 
+        anchor: new google.maps.Point(21, 70)
+    }; 
+     
+    
+    function initialize() { 
+      var mapOptions = { 
+        zoom: 14, 
+        center: coordData, 
+        scrollwheel: false, 
+        styles: styleArray 
+      } 
+    
+      var contentString = "<div></div>"; 
+      var infowindow = new google.maps.InfoWindow({ 
+        content: contentString, 
+        maxWidth: 200 
+      }); 
+       
+      var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions); 
+      marker = new google.maps.Marker({ 
+        map:map, 
+        draggable:true, 
+        position: coordData, 
+        icon: markerIcon
+      }); 
+       
+       
+    } 
+    google.maps.event.addDomListener(window, "load", initialize); 
+  }            
+  </script>
+</body>
+</html>
