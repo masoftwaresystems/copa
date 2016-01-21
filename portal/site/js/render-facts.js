271d26ce4b1d3e7267e43c32fbb39767363933cd
A.D. Slaton <slaton.ad@gmail.com>
2016-01-21 02:58:35 +0000

    Updating facts render

--- portal/site/js/render-facts.js
+++ portal/site/js/render-facts.js
@@ -1,20 +1,23 @@
+/*global Facts*/
+
 jQuery(document).ready(function () {
-    var Facts = Facts || {
+    var facts = Facts || {
         reports: {},
         findings: {},
         hazards: {},
         trained: {}
     };
     
-    if (Facts !== null && typeof Facts === 'undefined') {
-        var reports = Facts.reports.count || 0,
-            findings = Facts.findings.count || 0,
-            hazards = Facts.hazards.count || 0,
-            trained = Facts.trained.count || 0;
+    if (facts !== null && typeof facts === 'undefined') {
+        var reports = facts.reports.count || 0,
+            findings = facts.findings.count || 0,
+            hazards = facts.hazards.count || 0,
+            trained = facts.trained.count || 0;
             
         jQuery('#reports').html(reports);
         jQuery('#findings').html(findings);
         jQuery('#hazards').html(hazards);
         jQuery('#trained').html(trained);
     }
+    console.log('THE END');
 });
