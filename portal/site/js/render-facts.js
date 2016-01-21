31f8314a00ae859ec0b50ee2e37742ba15aef151
A.D. Slaton <slaton.ad@gmail.com>
2016-01-21 02:50:50 +0000

    Changing render facts to on dom ready

--- portal/site/js/render-facts.js
+++ portal/site/js/render-facts.js
@@ -1,18 +1,20 @@
-var Facts = Facts || {
-    reports: {},
-    findings: {},
-    hazards: {},
-    trained: {}
-};
-
-if (Facts !== null && typeof Facts === 'undefined') {
-    var reports = Facts.reports.count || 0,
-        findings = Facts.findings.count || 0,
-        hazards = Facts.hazards.count || 0,
-        trained = Facts.trained.count || 0;
-        
-    jQuery('#reports').html(reports);
-    jQuery('#findings').html(findings);
-    jQuery('#hazards').html(hazards);
-    jQuery('#trained').html(trained);
-}
+jQuery(document).ready(function () {
+    var Facts = Facts || {
+        reports: {},
+        findings: {},
+        hazards: {},
+        trained: {}
+    };
+    
+    if (Facts !== null && typeof Facts === 'undefined') {
+        var reports = Facts.reports.count || 0,
+            findings = Facts.findings.count || 0,
+            hazards = Facts.hazards.count || 0,
+            trained = Facts.trained.count || 0;
+            
+        jQuery('#reports').html(reports);
+        jQuery('#findings').html(findings);
+        jQuery('#hazards').html(hazards);
+        jQuery('#trained').html(trained);
+    }
+});
